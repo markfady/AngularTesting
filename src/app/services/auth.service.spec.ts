db16c91fcb68a7a6c0b47d479f0899ac239f5bb3
Markfady <markfady9@gmail.com>
2024-06-05 15:46:08 +0300

    Test case with mock data for our get method

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -2,6 +2,8 @@ import { TestBed } from "@angular/core/testing"
 import { AuthService, Post } from "./auth.service"
 import { LoginService } from "./login.service"
 import { HttpClientModule } from "@angular/common/http"
+import { HttpClientTestingModule , HttpTestingController } from "@angular/common/http/testing"
+
 /* Till video 7
 describe('check for is auth',()=>{
   let auth : AuthService
@@ -27,6 +29,7 @@ describe('check for is auth',()=>{
   })
 })
 */
+//testing service without mock data
 describe('testing get service',()=>{
   let service:AuthService
   beforeEach(()=>{
@@ -39,7 +42,39 @@ describe('testing get service',()=>{
     service.getPost(1).subscribe((post:Post)=>{
       console.log('data:',post)
       expect(post.id).toEqual(1);
-      done();
+      done()
     })
+  })
+})
+
+//testing service with mock data
+
+describe('testing http service using mock',()=>{
+  let service:AuthService
+  let httpMock:HttpTestingController
+    //setup your fake data to mock on it
+  const postMock={
+    userId:1,id:2,title:'myTitle',body:'myBody'
+  };
+  beforeEach(()=>{
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule] //Module to fake data with our service
+    })
+    service=TestBed.inject(AuthService);
+    httpMock=TestBed.inject(HttpTestingController);
+  })
+
+
+  it('getPost must get data as expected',()=>{ 
+    service.getPost(1).subscribe((data:Post)=>{
+      console.log('data Mock',data)
+      expect(data).toEqual(postMock)
+    })
+    //search if the url is done or not then return it's details
+    const request=httpMock.expectOne('https://jsonplaceholder.typicode.com/posts/1')
+    //send our fake data to the request (so when the request initiated we set our fake data to be returned)
+    request.flush(postMock)
+    //check if there are no request we didn't made
+    httpMock.verify()
   })
 })
