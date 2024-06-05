93678561f03202796bb171072eab5d85d461df56
Markfady <markfady9@gmail.com>
2024-06-05 12:19:57 +0300

    inject service , fixture , component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,26 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing"
 import { AppComponent } from "./app.component"
+import { AuthService } from "./services/auth.service"
 
-describe('clicking on hello function',()=>{
-    it('must return Hello World!',()=>{
-        const comp=new AppComponent()
-        expect(comp.hello()).toBe('Hello World')
+describe ('App component test',()=>{
+    let component:AppComponent
+    let fixture:ComponentFixture<AppComponent>
+    let authService:AuthService
+    beforeEach(function(){
+        fixture=TestBed.createComponent(AppComponent)  //wrap the component with it's template
+        component=fixture.componentInstance //instance from this component
+        authService=TestBed.inject(AuthService) //inject service
+    })
+    it('should create app component',()=>{
+        expect(component).toBeTruthy()
+    })
+    it('should have title ',()=>{
+        expect(component.title).toEqual('title')
+    })
+    it('return true user and password',()=>{
+        expect(component.canLogin('mark',123)).toBeTruthy()
+    })
+    it('return false when no user',()=>{
+        expect(component.canLogin('',123)).toBeFalsy()
     })
 })
