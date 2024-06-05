1554f1ce8d92670e3abee146646dd0178211ec74
Markfady <markfady9@gmail.com>
2024-06-05 12:48:13 +0300

    Post interface , get request

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 import { LoginService } from './login.service';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+export interface Post{
+  userId:number;
+  id:number;
+  title:string;
+  body:string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private loginService:LoginService) { }
+  constructor(private loginService:LoginService, private http:HttpClient) { }
   isAuth():boolean{
     // return !!localStorage.getItem('token') //if localStorage has token will return boolean value
     return this.loginService.isLogin();
@@ -17,5 +26,11 @@ export class AuthService {
     }else{
       return false
     }
+  }
+  //till video 7 comment this line to run test case of app component and other auth service
+  getPost(postId:number):Observable<Post>{
+    return this.http.get<Post>(
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    )
   }
 }
