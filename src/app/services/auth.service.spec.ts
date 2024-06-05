3dfa27c1af9f32398f30ebf94549690e3185aa9d
Markfady <markfady9@gmail.com>
2024-06-05 10:59:43 +0300

    usage of spy , testing dependant data

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,19 +1,26 @@
 import { AuthService } from "./auth.service"
+import { LoginService } from "./login.service"
 
 describe('check for is auth',()=>{
   let auth : AuthService
   beforeEach(()=>{ //before each test create new instance ( to not repeat this line each time)
-    auth=new AuthService();
+    // auth=new AuthService();
+    auth=new AuthService(new LoginService())
   })
-  afterEach(()=>{ //After each test the localStorage is clear
-    localStorage.removeItem('token')
-  })
-  it('must return true if there is a token in localStorage',()=>{
+  // afterEach(()=>{ //After each test the localStorage is clear
+  //   localStorage.removeItem('token')
+  // })
+  it('must return true if there is user logged in',()=>{
     localStorage.setItem('token','Hello World');
     expect(auth.isAuth()).toBeTruthy()
   })
-  it('must return false if there is not a token in localStorage',()=>{
+  it('must return false if there is not user logged in',()=>{
 
-    expect(auth.isAuth()).toBeFalsy();
+    expect(!auth.isAuth()).toBeFalsy();
+  })
+  it('getting fake data using spy',()=>{
+    const mySpy=jasmine.createSpyObj('',['isAuth']);
+    mySpy.isAuth.and.returnValue(new LoginService().isLogin()+'x')
+    expect(mySpy.isAuth()).toBe('truex','work data message for spy')
   })
 })
