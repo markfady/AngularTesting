618fe29c09fdb89d430e2c15faf713bfb3252ef4
Markfady <markfady9@gmail.com>
2024-06-06 12:06:39 +0300

    takes email and password as parameters

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, output } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'pm-login',
@@ -9,13 +10,16 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   isLoggedIn:boolean
+  @Output() submitData:EventEmitter<User>
   constructor(){
     this.isLoggedIn=false
+    this.submitData=new EventEmitter<User>() //instance from the output
   }
   ngOnInit(): void {}
 
-  login():void{
+  login(email:string,password:string):void{
     this.isLoggedIn=!this.isLoggedIn
+    this.submitData.emit({email,password}) //after clicking the submit button(login) we emit / send the data
   }
   get loginState():string{
     return `User is ${this.isLoggedIn? 'login' : 'logout'}`
